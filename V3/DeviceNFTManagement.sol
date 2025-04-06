5b8cc7d84467743d933a58e9741cb3d9febc484e
Anonymous <gramezan@gmail.com>
2025-04-06 02:33:55 -0700

    new NFT contract

--- V3/DeviceNFTManagement.sol
+++ V3/DeviceNFTManagement.sol
@@ -13,17 +13,21 @@ contract IoTDeviceNFT is ERC721Enumerable {
         string deviceType;
         string manufacturer;
         string deviceModel;
+        string ipfsMetadataURL;  // New field for IPFS metadata
     }
 
     // Mapping tokenId => DeviceInfo
     mapping(uint256 => DeviceInfo) public deviceDetails;
+    
+    // Mapping to check if a device with the same info already exists
+    mapping(bytes32 => bool) private deviceExists;
 
     constructor() ERC721("IoTDeviceNFT", "FDSIOT") {
         tokenCounter = 1;
     }
 
     /**
-     * @dev Mint an NFT for an IoT device.
+     * @dev Mint an NFT for an IoT device with IPFS metadata
      * The caller becomes the owner of the NFT.
      */
     function mintDeviceNFT(
@@ -31,8 +35,20 @@ contract IoTDeviceNFT is ERC721Enumerable {
         string memory deviceIdType,
         string memory deviceType,
         string memory manufacturer,
-        string memory deviceModel
+        string memory deviceModel,
+        string memory ipfsMetadataURL  // New parameter
     ) external returns (uint256) {
+        // Create a hash of the device info to check for uniqueness
+        bytes32 deviceHash = keccak256(abi.encodePacked(
+            deviceId,
+            deviceIdType,
+            deviceType,
+            manufacturer,
+            deviceModel
+        ));
+        
+        require(!deviceExists[deviceHash], "Device with these details already exists");
+
         uint256 newTokenId = tokenCounter;
 
         _safeMint(msg.sender, newTokenId);
@@ -42,8 +58,12 @@ contract IoTDeviceNFT is ERC721Enumerable {
             deviceIdType,
             deviceType,
             manufacturer,
-            deviceModel
+            deviceModel,
+            ipfsMetadataURL  // Store the IPFS URL
         );
+        
+        // Mark this device info as existing
+        deviceExists[deviceHash] = true;
 
         tokenCounter++;
         return newTokenId;
@@ -57,9 +77,10 @@ contract IoTDeviceNFT is ERC721Enumerable {
         string memory deviceIdType,
         string memory deviceType,
         string memory manufacturer,
-        string memory deviceModel
+        string memory deviceModel,
+        string memory ipfsMetadataURL  // New return value
     ) {
-        require(tokenCounter >= tokenId && tokenCounter > 1, "Token does not exist");
+        require(ownerOf(tokenId) != address(0), "Token does not exist");
 
         DeviceInfo memory info = deviceDetails[tokenId];
         return (
@@ -67,7 +88,16 @@ contract IoTDeviceNFT is ERC721Enumerable {
             info.deviceIdType,
             info.deviceType,
             info.manufacturer,
-            info.deviceModel
+            info.deviceModel,
+            info.ipfsMetadataURL  // Return the IPFS URL
         );
+    }
+
+    /**
+     * @dev Override tokenURI to return the IPFS metadata URL
+     */
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        require(ownerOf(tokenId) != address(0), "Token does not exist");
+        return deviceDetails[tokenId].ipfsMetadataURL;
     }
 }
